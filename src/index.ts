41b60de49384ccd70f289f54149ba17113853a60
Wataru Yahiro <wataru@WatarunoMacBook-puro.local>
2018-07-19 22:51:43 +0900

    call results API

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
-import { SheetService } from './sheet.service';
+import Folder = GoogleAppsScript.Drive.Folder;
+import { API } from './API';
 
 declare var global: any;
 
-global.createNewFile = (): void => {
-  const ss = SheetService.createInitialFile('New file');
-  ss.getRange('A2').setValue('Happy gas!');
+global.getScriptFolder = (): Folder => {
+  const scriptId = ScriptApp.getScriptId();
+  const file = DriveApp.getFileById(scriptId);
+  const folders = file.getParents();
+  while (folders.hasNext()) {
+    return folders.next();
+  }
+  return null;
+};
+
+global.getIksmSession = (folder: Folder): string => {
+  const files = folder.getFilesByName('iksm_session');
+  while (files.hasNext()) {
+    const file = files.next();
+    const text = file.getBlob().getDataAsString();
+    return text;
+  }
+  return null;
+};
+
+global.main = (): void => {
+  const folder = global.getScriptFolder();
+  const iksmSession = global.getIksmSession(folder);
+  const api = new API(iksmSession);
+  const response = api.callResults();
+  Logger.log(JSON.stringify(response));
 };
