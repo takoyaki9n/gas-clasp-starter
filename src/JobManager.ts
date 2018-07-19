bc5b31ba64e2372945b75660971eeee3aef0d9dc
Wataru Yahiro <wataru@WatarunoMacBook-puro.local>
2018-07-20 01:12:47 +0900

    refactor

--- src/JobManager.ts
+++ src/JobManager.ts
@@ -8,30 +8,30 @@ export class JobManager {
   private static IKSM_SESSION_FILE_NAME = 'iksm_session';
   private static LOCK_FILE_NAME = 'lock.txt';
 
-  private projectRoot: Folder;
+  private root: Folder;
   private lockFile: File;
   private api: API;
 
   constructor() {
-    this.projectRoot = DriveUtils.getScriptFolder();
+    this.root = DriveUtils.getScriptFolder();
     this.api = this.initApi();
   }
 
   private initApi() {
-    const iksmSessionFile = DriveUtils.getFile(this.projectRoot, JobManager.IKSM_SESSION_FILE_NAME);
+    const iksmSessionFile = DriveUtils.getFile(this.root, JobManager.IKSM_SESSION_FILE_NAME);
     const iksmSession = DriveUtils.getFileContentText(iksmSessionFile);
     return new API(iksmSession);
   }
 
   private lock(): File {
-    var lockFile = DriveUtils.getFile(this.projectRoot, JobManager.LOCK_FILE_NAME);
+    var lockFile = DriveUtils.getFile(this.root, JobManager.LOCK_FILE_NAME);
     if (lockFile != null) return null;
-    lockFile = this.projectRoot.createFile(JobManager.LOCK_FILE_NAME, '');
+    lockFile = this.root.createFile(JobManager.LOCK_FILE_NAME, '');
     return lockFile != null ? lockFile : null;
   }
 
   private unlock() {
-    this.projectRoot.removeFile(this.lockFile);
+    this.root.removeFile(this.lockFile);
   }
 
   public run(): void {
